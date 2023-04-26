3da5f4a7894cac16c6cd001694c9d5a4229c7113
Sudhanshu Biswal <87547209+Sudhanshu-Biswal@users.noreply.github.com>
2023-04-26 06:44:11 +0530

    Update forecastapp.py

--- forecastapp.py
+++ forecastapp.py
@@ -44,15 +44,5 @@ def load_csv(input):
     return df_input
 
 
-def prep_data(df):
-    df_input = df.rename({date_col: "ds", metric_col: "y"}, errors='raise', axis=1)
-    st.markdown("The selected date column is now labeled as **ds** and the values columns as **y**")
-    df_input = df_input[['ds', 'y']]
-    df_input = df_input.sort_values(by='ds', ascending=True)
-    return df_input
 
-df = prep_data(df)                     
-st.dataframe(df)
-
-st.write(df.describe())
 
